6b6244223f0bb0d99a1e54221bd97caa65212d46
aathiraanil1997 <114383465+Aathira1997@users.noreply.github.com>
2024-09-30 17:18:19 +0530

    Create lib.py

---
+++ lib.py
@@ -0,0 +1,8 @@
+!pip install jiwer
+from jiwer import wer
+import os
+import random
+from glob import glob
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
