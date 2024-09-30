ed6832cb26230da1c9d5d05506c61c0d14c65a2a
aathiraanil1997 <114383465+Aathira1997@users.noreply.github.com>
2024-09-30 17:18:59 +0530

    Create token embedding.py

---
+++ token embedding.py
@@ -0,0 +1,12 @@
+class TokenEmbedding(layers.Layer):
+    def __init__(self, num_vocab=1000, maxlen=100, num_hid=64):
+        super().__init__()
+        self.emb = tf.keras.layers.Embedding(num_vocab, num_hid)
+        self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=num_hid)
+
+    def call(self, x):
+        maxlen = tf.shape(x)[-1]
+        x = self.emb(x)
+        positions = tf.range(start=0, limit=maxlen, delta=1)
+        positions = self.pos_emb(positions)
+        return x + positions
